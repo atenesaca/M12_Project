0ba5dc27ffcbe1a1243476f563e6834eb93b94f0
Bort86 <dinocolmillo@gmail.com>
2019-05-12 21:34:37 +0200

    Added plots to code

--- CellProject/app.R
+++ CellProject/app.R
@@ -1,5 +1,13 @@
 ## app.R ##
 library(shinydashboard)
+library(Biobase)
+library(GEOquery)
+library("reshape2")
+library(ggplot2)
+library(dplyr)
+library(limma)
+library(annotate)
+library(shinycssloaders)
 
 ui <- dashboardPage(
   skin = "red",
@@ -36,12 +44,90 @@ ui <- dashboardPage(
     ),
     conditionalPanel(
       condition= "input.sidebar == 'plot'",
-      plotOutput("plot")
+      plotOutput("plot") %>% withSpinner(),
+      plotOutput("plot2") %>% withSpinner()
     )
   )
 )
 
 server <- function(input, output) {
+  
+  ##### DATA #####
+  
+  # retrieving query
+  uQuery <- eventReactive(input$search, {
+    query <- input$queryId
+    data <- getGEO(query, destdir=".")
+    return(data)
+  })
+  
+  # normalizing query
+  eset <- reactive({
+    req(uQuery)
+    e <- uQuery()
+    e <- GDS2eSet(e, do.log2=TRUE)
+    e
+  })
+  
+  # groups, this will be choosen by user
+  groups <- reactive({
+    req(eset())
+    x <- eset()
+    pData(x)[,2]
+  }) 
+
+  # dataframe for plot1
+  genes <- reactive({
+    req(eset(), groups())
+    sample <- sampleNames(eset())
+    y <- exprs(eset())
+    groups<- groups()
+    g<- melt(y, varnames = c( "probe", "sample"))
+    g$genotype <- groups[match(g$sample, sample)]
+    return(g)
+  })
+  
+  # data for the heatmap
+  ebayes <- reactive({
+    req(groups(), eset())
+    y <- exprs(eset())
+    g <- groups()
+    design <- model.matrix(~factor(g))
+    fit <- lmFit(y, design)
+    return(eBayes(fit))
+  })
+  
+  tab <- reactive({
+    req(ebayes())
+    e <- ebayes()
+    return(topTable(e, coef=2, adjust="fdr", n=150))
+  })
+  
+  labCol<- reactive({
+    req(groups())
+    g <- groups()
+    g <- as.character(g)
+    return(g)
+  }) 
+  
+  ########## PLOTS #########
+  
+  ### Plot 1
+  output$plot <- renderPlot({
+    req(genes())
+    data <- genes()
+    ggplot(data, aes(x=sample, y=value, fill=genotype)) + geom_boxplot()
+  })
+  
+  ### Plot 2
+  output$plot2 <- renderPlot({
+    req(eset())
+    y<- exprs(eset())
+    tab <- tab()
+    labCol<- labCol()
+    heatmap(y[row.names(tab),], labCol = labCol, scale="none", cexRow=0.5)
+  })
+  
 }
 
 shinyApp(ui, server)
